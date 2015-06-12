b59a8a724e97d471af92cfcef6f5aaa4970fe45b
Krishnamoorthy Damodaran <kittu_14@yahoo.com>
2015-06-11 23:31:01 -0500

    'running'

---
+++ TomcatEmbededServlet/src/main/java/org/yazh/embeded/tomcat/ServletRunner.java
@@ -0,0 +1,27 @@
+package org.yazh.embeded.tomcat;
+
+import java.io.File; 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import org.yazh.basic.SimpleServlet;
+
+public class ServletRunner {
+	public static void main(String args[]){
+		try {
+		Tomcat tomcat = new Tomcat();
+		tomcat.setPort(8080);
+		File base = new File(System.getProperty("java.io.tmpdir"));
+		Context rootCtx = tomcat.addContext("/app", base.getAbsolutePath());
+		Tomcat.addServlet(rootCtx, "SimpleServlet", new SimpleServlet());
+		rootCtx.addServletMapping("/date", "SimpleServlet");
+		tomcat.start();
+		tomcat.getServer().await();
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
