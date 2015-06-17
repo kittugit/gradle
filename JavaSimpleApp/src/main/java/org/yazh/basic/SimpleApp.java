e513234cd2cdff1d01119adb9b5fbf0ba8ca76e2
Krishnamoorthy Damodaran <kittu_14@yahoo.com>
2015-06-16 22:15:12 -0500

    'application'

--- JavaSimpleApp/src/main/java/org/yazh/basic/SimpleApp.java
+++ JavaSimpleApp/src/main/java/org/yazh/basic/SimpleApp.java
@@ -3,6 +3,6 @@ package org.yazh.basic;
 public class SimpleApp {
 
 	public static void main(String args[]){
-		System.out.println("App started");
+		System.out.println("Application started");
 	}
 }
