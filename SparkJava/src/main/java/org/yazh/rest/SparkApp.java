ec2ceb240f78a31224a800cc231e71b125dc2699
Krishnamoorthy Damodaran <kittu_14@yahoo.com>
2015-07-22 23:29:24 -0500

    'contenttype'

--- SparkJava/src/main/java/org/yazh/rest/SparkApp.java
+++ SparkJava/src/main/java/org/yazh/rest/SparkApp.java
@@ -5,15 +5,14 @@ import com.google.gson.Gson;
 public class SparkApp {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
+		
 		Gson gson = new Gson();
 		post("/hello", "application/json", (req, res) -> {
-		
-		Name name = gson.fromJson(req.body(), Name.class);
-		name.toUpperCase();
-		return name;
-		
-		
-		}, gson::toJson); 
+				Name name = gson.fromJson(req.body(), Name.class);
+				name.toUpperCase();
+				res.type("application/json");
+				return name;
+			}, gson::toJson); 
     }
 }
 
