8545c1f71270ac98f2a63b42224a526fff7f215f
Krishnamoorthy Damodaran <kittu_14@yahoo.com>
2015-07-22 23:22:37 -0500

    'gson'

---
+++ SparkJava/src/main/java/org/yazh/rest/Name.java
@@ -0,0 +1,31 @@
+package org.yazh.rest;
+
+public class Name {
+  private String fName;
+  private String lName;
+  
+  public String getFName(){
+	return fName;
+  }
+  
+  public void setFName(String fName){
+	this.fName = fName;
+  }
+  
+  public String getLName(){
+	return lName;
+  }
+  
+  public void setLName(String lName){
+	this.lName = lName;
+  }
+  
+  public void toUpperCase() {
+	fName = fName.toUpperCase();
+	lName = lName.toUpperCase();
+	
+  }
+  
+  
+
+}
--- SparkJava/src/main/java/org/yazh/rest/SparkApp.java
+++ SparkJava/src/main/java/org/yazh/rest/SparkApp.java
@@ -1,9 +1,19 @@
 package org.yazh.rest;
 import static spark.Spark.*;
+import com.google.gson.Gson;
 
 public class SparkApp {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
+		Gson gson = new Gson();
+		post("/hello", "application/json", (req, res) -> {
+		
+		Name name = gson.fromJson(req.body(), Name.class);
+		name.toUpperCase();
+		return name;
+		
+		
+		}, gson::toJson); 
     }
 }
 
